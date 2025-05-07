6195c83bc829f94c82702e43d3ce5647092d45d4
natalio silveira <nataliosilveira@yahoo.com.br>
2025-05-07 17:25:54 -0300

    feat: update banners

--- src/app/components/Carrousel/index.tsx
+++ src/app/components/Carrousel/index.tsx
@@ -7,10 +7,44 @@ const Carrousel: React.FC = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <Image src={'/newbanner.jpg'} width={1440} height={811} alt="banner principal" aria-hidden className="w-full" />
+        <Image
+          src={'/desktop/banner01.jpg'}
+          width={1440}
+          height={811}
+          alt="banner principal"
+          aria-hidden
+          className="w-full"
+        />
       </Carousel.Item>
       <Carousel.Item>
-        <Image src={'/newbanner.jpg'} width={1440} height={811} alt="banner principal" aria-hidden className="w-full" />
+        <Image
+          src={'/desktop/banner02.jpg'}
+          width={1440}
+          height={811}
+          alt="banner principal"
+          aria-hidden
+          className="w-full"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src={'/desktop/banner03.jpg'}
+          width={1440}
+          height={811}
+          alt="banner principal"
+          aria-hidden
+          className="w-full"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src={'/desktop/banner04.jpg'}
+          width={1440}
+          height={811}
+          alt="banner principal"
+          aria-hidden
+          className="w-full"
+        />
       </Carousel.Item>
     </Carousel>
   );
