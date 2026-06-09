9cf62e987d49e2716b48fbd5921763af0be55814
natalio silveira <nataliosilveira@yahoo.com.br>
2026-06-09 14:28:05 -0300

    feat: update WhatsApp link and add target attributes for redirect functionality

--- src/app/components/Whatsapp/index.tsx
+++ src/app/components/Whatsapp/index.tsx
@@ -8,7 +8,7 @@ interface WhatsappProps {
 const Whatsapp: React.FC<WhatsappProps> = ({ title }) => {
   return (
     <div>
-      <a href="#">
+      <a href="https://wa.me/551141618439" target="_blank" rel="noopener noreferrer">
         <div className="bold bg-[#15A960] rounded-full py-2 px-10 flex items-center gap-2 text-white font-bold text-lg justify-center">
           <Phone /> <span>{title}</span>
         </div>
